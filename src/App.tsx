e84e9bcdeea70d0a12b8ebf44a99f77a4df115af
Gergics Milan <gergics.milan@arukereso.com>
2025-02-10 18:14:59 +0100

    feature - add query client provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import BrowserSwitch from "./routes/BrowserSwitch";
 
-const App = () => <BrowserSwitch />;
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: false, staleTime: 3600000 } },
+});
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <BrowserSwitch />
+  </QueryClientProvider>
+);
 
 export default App;
